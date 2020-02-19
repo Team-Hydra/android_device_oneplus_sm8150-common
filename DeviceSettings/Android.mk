409b5f470212cd97a1db60f72bd77d81658ba355
Vachounet <vachounet@live.fr>
2020-02-19 19:28:08 +0200

    sm8150-common: Set client package name
    * Introduce a new boot receiver that forces camera client package name via HIDL HAL to fix portrait mode
    
    Change-Id: Ibac00612b794eefd3c6d7ba6fa4e44fcc0197cc1
    
     dynamicaly set packageName
    
    Change-Id: I5d14d24d0a9a3acc7d219ea2f44f7c87ad8fef20
    
     oneplus6: cleanup OnePlus camera provider interface
    
    Change-Id: I2f14be97c8f20a6bdb0d489bc0becc20b2708fba
    
    Signed-off-by: chandra <chandra.chaganti@hotmail.com>

--- DeviceSettings/Android.mk
+++ DeviceSettings/Android.mk
@@ -31,6 +31,7 @@ LOCAL_STATIC_ANDROID_LIBRARIES := \
     androidx.preference_preference
 
 LOCAL_STATIC_JAVA_LIBRARIES := \
+    vendor.oneplus.camera.CameraHIDL-V1.0-java \
     vendor.oneplus.hardware.display-V1.0-java
 
 LOCAL_RESOURCE_DIR := \
