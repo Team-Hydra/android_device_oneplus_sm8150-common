fc6d49499286a90d82ec0a06aa94e170dcf7fd66
Matt Filetto <matt.filetto@gmail.com>
2020-10-21 13:32:45 -0700

    [TEMP] DeviceSettings: Fix compile errors
    * Some features aren't merged to fw/base yet
    
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- DeviceSettings/src/com/aosip/device/DeviceSettings/Startup.java
+++ DeviceSettings/src/com/aosip/device/DeviceSettings/Startup.java
@@ -25,7 +25,6 @@ import android.provider.Settings;
 import android.text.TextUtils;
 import androidx.preference.PreferenceManager;
 
-import com.aosip.device.DeviceSettings.TouchscreenGestureSettings;
 
 public class Startup extends BroadcastReceiver {
 
@@ -51,7 +50,7 @@ public class Startup extends BroadcastReceiver {
     @Override
     public void onReceive(final Context context, final Intent bootintent) {
         boolean enabled = false;
-        TouchscreenGestureSettings.MainSettingsFragment.restoreTouchscreenGestureStates(context);
+        
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
         enabled = sharedPrefs.getBoolean(DeviceSettings.KEY_SRGB_SWITCH, false);
         restore(SRGBModeSwitch.getFile(), enabled);
