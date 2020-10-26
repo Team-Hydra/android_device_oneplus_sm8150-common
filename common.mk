a4ef2cc803eeea091c24954bfaad77424bb10aa9
Matt Filetto <matt.filetto@gmail.com>
2020-10-26 04:31:33 -0700

    revert some display hal stuff to yaap base
    Change-Id: Ibd541dba53e13aacbf9171a459effa66f8ebbae0

--- common.mk
+++ common.mk
@@ -218,7 +218,7 @@ PRODUCT_COPY_FILES += \
 
 # Display
 PRODUCT_PACKAGES += \
-    android.hardware.graphics.composer@2.3-service \
+    android.hardware.graphics.composer@2.2-service \
     android.hardware.graphics.mapper@2.0-impl-qti-display \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
@@ -234,8 +234,7 @@ PRODUCT_PACKAGES += \
     vendor.display.config@1.7 \
     vendor.qti.hardware.display.allocator-service \
     vendor.qti.hardware.display.allocator@1.0-service \
-    vendor.qti.hardware.display.allocator@1.0.vendor \
-    vendor.qti.hardware.display.mapper@1.0.vendor
+    vendor.qti.hardware.display.allocator@1.0.vendor
 
 # Doze
 PRODUCT_PACKAGES += \
