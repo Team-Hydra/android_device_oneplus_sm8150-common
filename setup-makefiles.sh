fb10fbe80720c15c56e0d2a7a72dd463a02bec1e
Matt Filetto <matt.filetto@gmail.com>
2021-02-28 03:19:26 -0800

    sm8150-common: small cleanups
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>
    Change-Id: I4397a91e8a5a2b7e0b5abffe859bd6a0e67cde2a

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 #
-# Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2021 The LineageOS Project
+# Copyright (C) 2018-2019 The LineageOS Project
 #
-# SPDX-License-Identifier: Apache-2.0
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 
 set -e
@@ -12,9 +21,9 @@ set -e
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-ANDROID_ROOT="$MY_DIR"/../../..
+YAAP_ROOT="${MY_DIR}/../../.."
 
-HELPER="$ANDROID_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+HELPER="${YAAP_ROOT}/vendor/yaap/build/tools/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
@@ -22,9 +31,9 @@ fi
 source "${HELPER}"
 
 # Initialize the helper for common
-setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "$ANDROID_ROOT" true
+setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${YAAP_ROOT}" true
 
-# Warning headers and guards
+# Copyright headers and guards
 write_headers "guacamole guacamoleb guacamoleg hotdog hotdogb hotdogg"
 
 # The standard common blobs
@@ -36,7 +45,7 @@ write_footers
 if [ -s "${MY_DIR}/../${DEVICE}/proprietary-files.txt" ]; then
     # Reinitialize the helper for device
     INITIAL_COPYRIGHT_YEAR="$DEVICE_BRINGUP_YEAR"
-    setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false
+    setup_vendor "${DEVICE}" "${VENDOR}" "${YAAP_ROOT}" false
 
     # Copyright headers and guards
     write_headers
